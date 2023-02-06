bf666e4af244633596c4d165482b0922d9d59dfd
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-06 23:47:48 +0600

    Configure redux logger and devtools in store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
 });

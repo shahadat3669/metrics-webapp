5bf45a84fd2390b8ebfeb3ef8ee0a9d47a0f9077
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-08 00:10:28 +0600

    Add html skeleton for header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { FaMicrophone, FaCog, FaChevronLeft } from 'react-icons/fa';
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <header>
+    <div>
+      <FaChevronLeft />
+      2015
+    </div>
+    <div>
+      <p>most views</p>
+    </div>
+    <div>
+      <FaMicrophone />
+      <FaCog />
+    </div>
+  </header>
+);
 
 export default Header;

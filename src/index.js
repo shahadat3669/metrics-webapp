a632195a19a6a8ecbb508c3c1d0ba0ded129a6fc
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-07 23:25:17 +0600

    Add index.scss in index js file.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
+import './styles/index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);

7ba73021bc183ac349a09b444641f08149534dd3
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-08 00:30:42 +0600

    Conditionally show the header text as per design.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { FaMicrophone, FaCog, FaChevronLeft } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom';
 import styles from '../styles/components/Header.module.scss';
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.header__item}>
-      <FaChevronLeft className={styles.header__icon_btn} />
-      2015
-    </div>
-    <div className={styles.header__item}>
-      <p>most views</p>
-    </div>
-    <div
-      className={`${styles.header__item} ${styles['header__item--gap-hug']}`}
-    >
-      <FaMicrophone className={styles.header__icon_btn} />
-      <FaCog className={styles.header__icon_btn} />
-    </div>
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
+  console.log();
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__item}>
+        <FaChevronLeft className={styles.header__icon_btn} />
+        2015
+      </div>
+      <div className={styles.header__item}>
+        <p>
+          {location.pathname === '/about' ? 'town/city views' : 'most views'}
+        </p>
+      </div>
+      <div
+        className={`${styles.header__item} ${styles['header__item--gap-hug']}`}
+      >
+        <FaMicrophone className={styles.header__icon_btn} />
+        <FaCog className={styles.header__icon_btn} />
+      </div>
+    </header>
+  );
+};
 
 export default Header;

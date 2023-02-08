d6e6face39f2adf1387a041d11da22dcfc4c0d6b
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-08 22:11:27 +0600

    Add Category component in home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Category from '../components/Category';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <div>
+    <Category />
+  </div>
+);
 
 export default Home;

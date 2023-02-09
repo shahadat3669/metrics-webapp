59aa01531cdb2ad1201f778004119a3d9d87a930
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-10 03:56:57 +0600

    Add testing for pending state.

--- src/__test__/features/countriesSlice.test.js
+++ src/__test__/features/countriesSlice.test.js
@@ -1,9 +1,23 @@
 import store from '../../app/store';
+import reducer from '../../features/countries/countriesSlice';
 
 describe('Countries redux state tests', () => {
   it('Initial State', () => {
     const state = store.getState().countries;
     expect(state.status).toEqual('idle');
     expect(state.countries).toEqual([]);
+  });
+
+  test('Countries pending state', () => {
+    const prevState = { status: 'idle', countries: [], error: null };
+    const action = {
+      type: 'countries/FETCH_COUNTRIES/pending',
+    };
+    const state = reducer(prevState, action);
+    expect(state).toEqual({
+      status: 'loading',
+      countries: [],
+      error: null,
+    });
   });
 });

73dd4ed5b271f1ccfc71c39ed5d23f9b24798d62
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-10 02:41:19 +0600

    Add countriesReducer in redux store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import countriesReducer from '../features/countries/countriesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    countries: countriesReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
